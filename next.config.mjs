4ddc8ed0aed573cbda15d943a32852635bfdf9ad
hariadiarief <adi.arief77@gmail.com>
2024-06-12 12:01:56 +0700

    next-pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  cacheOnFrontendNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  disable: false,
+});
+
 const nextConfig = {};
 
-export default nextConfig;
+
+export default withPWA(nextConfig);
