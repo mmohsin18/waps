49f01c8be5028b3c39949eec8524f681809f6281
hariadiarief <adi.arief77@gmail.com>
2024-06-12 12:51:47 +0700

    offline page

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,18 @@
 import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
+  // disable: process.env.NODE_ENV === "development",
   dest: "public",
   cacheOnFrontendNav: true,
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
   disable: false,
+  fallbacks: {
+    document: "/~offline"
+  },
+  workboxOptions: {
+    disableDevLogs: true,
+  },
 });
 
 const nextConfig = {};
