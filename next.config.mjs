bb79f576943d724f371936b9faec01454166b386
hariadiarief <adi.arief77@gmail.com>
2024-06-24 16:19:41 +0700

    offline handle

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ import withSerwistInit from "@serwist/next";
 const withSerwist = withSerwistInit({
     swSrc: "src/app/sw.ts",
     swDest: "public/sw.js",
+    reloadOnOnline: true,
 });
 
 const nextConfig = {}
--- src/app/sw.ts
+++ src/app/sw.ts
@@ -1,6 +1,6 @@
-import { defaultCache } from "@serwist/next/worker";
-import type { PrecacheEntry, SerwistGlobalConfig } from "serwist";
-import { Serwist } from "serwist";
+import { defaultCache } from '@serwist/next/worker'
+import type { PrecacheEntry, SerwistGlobalConfig } from 'serwist'
+import { Serwist } from 'serwist'
 
 // This declares the value of `injectionPoint` to TypeScript.
 // `injectionPoint` is the string that will be replaced by the
@@ -8,11 +8,11 @@ import { Serwist } from "serwist";
 // `"self.__SW_MANIFEST"`.
 declare global {
   interface WorkerGlobalScope extends SerwistGlobalConfig {
-    __SW_MANIFEST: (PrecacheEntry | string)[] | undefined;
+    __SW_MANIFEST: (PrecacheEntry | string)[] | undefined
   }
 }
 
-declare const self: ServiceWorkerGlobalScope;
+declare const self: ServiceWorkerGlobalScope
 
 const serwist = new Serwist({
   precacheEntries: self.__SW_MANIFEST,
@@ -20,6 +20,16 @@ const serwist = new Serwist({
   clientsClaim: true,
   navigationPreload: true,
   runtimeCaching: defaultCache,
-});
+  fallbacks: {
+    entries: [
+      {
+        url: '/~offline', // the page that'll display if user goes offline
+        matcher({ request }) {
+          return request.destination === 'document'
+        }
+      }
+    ]
+  }
+})
 
-serwist.addEventListeners();
+serwist.addEventListeners()
